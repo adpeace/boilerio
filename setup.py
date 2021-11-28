7039850c5693051576e1ba17d8929cab7bca218e
Andrew Peace <andrew.peace@cantab.net>
2021-11-28 00:48:46 +0000

    Use README.md directly for long description.

--- setup.py
+++ setup.py
@@ -1,14 +1,9 @@
 import subprocess
 from setuptools import setup, find_packages
 
-# Try to create an rst long_description from README.md:
-try:
-    args = 'pandoc', '--to', 'rst', 'README.md'
-    long_description = subprocess.check_output(args)
-    long_description = long_description.decode()
-except Exception as error:
-    print("WARNING: Couldn't generate long_description - is pandoc installed?")
-    long_description = None
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(name='boilerio',
       version='0.0.5',
@@ -18,6 +13,7 @@ setup(name='boilerio',
       author_email='andrew.peace@gmail.com',
       license='MIT',
       long_description=long_description,
+      long_description_content_type='text/markdown',
       classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'License :: OSI Approved :: MIT License',
